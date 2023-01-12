9e89e9ecad0967683b1b74ddaf4621705e870d3a
xuzhanyi <xuzhanyi@joyy.com>
2023-01-12 23:20:26 +0800

    feat: 增加服务启动提示

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -12,6 +12,10 @@ install(fastify)
 const start = async () => {
   try {
     await fastify.listen({ port: 3000 })
+    console.log(`\
+  🚀 Server ready at: http://localhost:3000/graphiql
+  ⭐️ Graphql client running at: http://localhost:3000/altair
+  `)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
